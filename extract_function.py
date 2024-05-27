14b5f9aba2562badb842857ad608296348bb1b4c
ucasqsl <qinsiliang18@mails.ucas.ac.cn>
2024-05-28 00:09:18 +0800

    fix overlapping extent

--- extract_function.py
+++ extract_function.py
@@ -44,7 +44,36 @@ def get_signature(node):
         signature = re.sub(r'static ', '', signature)
         
     return signature
-            
+
+def merge_extents(extents, lines):
+    # print(extents)
+    # converting list of extents to non-overlapping extents
+    lines_to_offset = {}
+    current_offset = 0
+    for i, line in enumerate(lines):
+        lines_to_offset[i+1] = current_offset
+        current_offset += len(line)
+    extent_ranges = []
+    for extent in extents:
+        extent_start_offset = lines_to_offset[extent.start.line] + extent.start.column - 1
+        extent_end_offset = lines_to_offset[extent.end.line] + extent.end.column
+        extent_ranges.append((extent_start_offset, extent_end_offset))
+    extent_ranges.sort(key=lambda x: x[0])
+    merged_extents = []
+    for extent_range in extent_ranges:
+        if not merged_extents:
+            merged_extents.append(extent_range)
+        else:
+            last_extent = merged_extents[-1]
+            if extent_range[0] > last_extent[1]:
+                merged_extents.append(extent_range)
+            else:
+                merged_extents[-1] = (last_extent[0], max(last_extent[1], extent_range[1]))
+    content = "".join(lines)
+    merged_result = ";\n".join([content[start:end] if not content[start:end].startswith('static') else re.sub(r'static ', '', content[start:end]) for start, end in merged_extents])
+    # print(merged_extents)
+    return merged_extents, merged_result
+    
 def extract(source, function_name, output):
     if not os.path.exists(output):
         os.makedirs(output, exist_ok=True)
@@ -52,9 +81,9 @@ def extract(source, function_name, output):
     file = Path(source)
     tu = parser.parse(file.absolute(), options=cindex.TranslationUnit.PARSE_DETAILED_PROCESSING_RECORD)
     header_lines = []
-    function_lines = []
-    remaining_lines = []
-    include_lines = []
+    function_extents = []
+    remaining_extents = []
+    include_extents = []
     # print(tu.cursor.kind, tu.cursor.spelling, tu.cursor.location.file)
     with open(source, 'r') as f:
         lines = f.readlines()
@@ -66,44 +95,42 @@ def extract(source, function_name, output):
             # if no need to dive into header files
             continue
         
-        print(node.kind, node.spelling, node.location.file)
+        # print(node.kind, node.spelling, node.location.file, node.is_definition(), node.kind.is_statement(), node.extent)
         if node.kind == cindex.CursorKind.INCLUSION_DIRECTIVE:
-            include_lines.append(get_node_content(lines, node))
+            include_extents.append(node.extent)
             
         elif node.kind in [cindex.CursorKind.STRUCT_DECL, cindex.CursorKind.UNION_DECL, cindex.CursorKind.ENUM_DECL, cindex.CursorKind.TYPEDEF_DECL, cindex.CursorKind.VAR_DECL]:
-            include_lines.append(get_node_content(lines, node))
+            include_extents.append(node.extent)
 
         elif node.kind == cindex.CursorKind.FUNCTION_DECL:
-            
-            
-            current_function = get_node_content(lines, node)
-            if node.storage_class == cindex.StorageClass.STATIC:
-                # find the first occurrence of 'static' and remove it
-                current_function = re.sub(r'static ', '', current_function)
-            if node.spelling == function_name:
-                function_lines.append(current_function)
+            if node.is_definition():
+                current_function = get_node_content(lines, node)
+                if node.storage_class == cindex.StorageClass.STATIC:
+                    # find the first occurrence of 'static' and remove it
+                    current_function = re.sub(r'static ', '', current_function)
+                if node.spelling == function_name:
+                    function_extents.append(node.extent)
+                else:
+                    if node.spelling == "LLVMFuzzerTestOneInput" or node.spelling == "main":
+                        continue
+                    remaining_extents.append(node.extent)
+                signature = get_signature(node)
+                if not signature.startswith("extern"):
+                    signature = f"extern {signature}"
+                header_lines.append(f"{signature};\n")
             else:
-                remaining_lines.append(current_function)
-            signature = get_signature(node)
-            header_lines.append(f"extern {signature};\n")
+                include_extents.append(node.extent)
         else:
-            include_lines.append(get_node_content(lines, node))
-
-    # Add the includes to the header file
-    header_lines = include_lines + header_lines
-
-    # Add the include for the header file to the function and remaining files
-    function_lines = [f'#include "{header_output}"\n'] + function_lines
-    remaining_lines = [f'#include "{header_output}"\n'] + remaining_lines
-
+            include_extents.append(node.extent)
+            
     with open(header_output, 'w') as f:
-        f.writelines(header_lines)
+        f.write(merge_extents(include_extents, lines)[1] + "\n" + "".join(header_lines))
 
     with open(function_output, 'w') as f:
-        f.writelines(function_lines)
+        f.write(f'#include "header.h"\n' + merge_extents(function_extents, lines)[1])
 
     with open(remaining_output, 'w') as f:
-        f.writelines(remaining_lines)
+        f.write(f'#include "header.h"\n' + merge_extents(remaining_extents, lines)[1])
     return header_output, function_output, remaining_output
         
 def main():
